029fd3c3eb0c92244204dd303a83995adf06d6b3
CoyotWilly <coyotwilly@gmail.com>
2023-01-15 22:34:21 +0100

    plotting added and file write init code

--- logger.py
+++ logger.py
@@ -1,36 +1,33 @@
 import numpy as np
 import serial.tools.list_ports
+import time
 from time import sleep
 import json
 import matplotlib.pyplot as plt
 import keyboard
+import csv
 
 
-def plotting(time, temperature_plt, ref_plt, u_plt, error_plt):
-
+def plotting(time_plt, temperature_plt, ref_plt, u_plt, error_plt):
     plt.clf()
     plt.subplot(3, 1, 1)
-    plt.plot(time, temperature_plt, time, ref_plt)
+    plt.plot(time_plt, temperature_plt, time, ref_plt)
     plt.title("BMP280 logger")
     plt.ylabel("Temperature [C]")
     plt.legend(["Current", "Reference point"])
-    # plt.ylim([min(temperature_plt) - 1, max(temperature_plt) + 1])
-    plt.grid()
+    plt.xticks([])
 
     plt.subplot(3, 1, 2)
-    plt.plot(time, u_plt)
+    plt.plot(time_plt, u_plt)
     plt.legend('Control signal')
     plt.ylabel("PWM Duty [%]")
-    plt.ylim([-1, 101])
-    plt.grid()
+    plt.xticks([])
 
     plt.subplot(3, 1, 3)
-    plt.plot(time, error_plt)
+    plt.plot(time_plt, error_plt)
     plt.legend('Error')
     plt.ylabel("Temperature [C]")
     plt.xlabel("Time [s]")
-    # plt.ylim([0.1, max(error_plt)])
-    plt.grid()
 
     plt.show()
     plt.pause(0.0001)
@@ -43,10 +40,16 @@ for i in port:
     ports.append(str(i))
     print(str(i))
 
-plt.ion()
+# create a file
+# time_str = time.strftime("%Y%m%d-%H%M%S")
+# file = with open("data_open_loop_object%s.csv" % time_str, "w")
 
+# file_csv = csv.DictWriter(file, fieldnames=name)
 # receiver assigment
 receiver = serial.Serial('COM6', 115200, timeout=1, parity=serial.PARITY_NONE)
+
+# enable plot update
+plt.ion()
 
 # time declaration
 t = []
@@ -58,11 +61,11 @@ u = []
 ref = []
 error = []
 sample = []
-
+u_prev = 10000
 while True:
     text = receiver.readline()
     temp = [0, 0, 0, 0]
-    u_prev = 10000
+
     try:
         sample = json.loads(text)
         temp = [sample["temperature"], sample["ref"], sample["u"], sample["error"]]
@@ -72,12 +75,14 @@ while True:
             u_prev = temp[2]
     except ValueError:
         print("Bad JSON\n")
-        # print(temp)
+        print(temp)
         receiver.flush()
         receiver.reset_input_buffer()
+    # file.write("%.2f," % float(temp[0]))
+    file.writeheader
     temperature.append(temp[0])
-    u.append(temp[1])
-    ref.append(temp[2])
+    ref.append(temp[1])
+    u.append(temp[2])
     error.append(temp[3])
     t.append(t_max)
     t_max += 1
@@ -88,4 +93,4 @@ while True:
         break
 
 receiver.close()
-
+file.close()
